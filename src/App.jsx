5e574911f07e0908bc2cdc4f9be69e6fdf626c43
Unmey Mahaddalkar <unmey08@gmail.com>
2025-02-27 13:02:43 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline">Component</h2>
+    </>
+  );
+};
+
+export default App;
