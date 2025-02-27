496f9fa4280760bdf4e20b3b4be2eee81fda6e85
Unmey Mahaddalkar <unmey08@gmail.com>
2025-02-27 13:09:37 -0800

    App header

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import "./App.css";
 
 const App = () => {
   return (
-    <>
-      <h2 className="text-3xl font-bold underline">Component</h2>
-    </>
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
   );
 };
 
